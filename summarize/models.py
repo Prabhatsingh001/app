b090627a54cf91ba11179c4aa010fc4033aa8d42
Ghosty <87269080+Prabhatsingh001@users.noreply.github.com>
2024-12-20 04:04:08 +0530

    Update models.py

--- summarize/models.py
+++ summarize/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+import datetime
 
 # Create your models here.
+class summarize_model(models.Model):
+    text = models.TextField()
+    summary = models.TextField()
+    date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.text
